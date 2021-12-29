03d202b20290334116147b9372d6ae0297b83313
Amged <amgedelshiekh@gmil.com>
2021-12-29 11:45:49 +0900

    initialize day 18

---
+++ Day 18 Turtle/main.py
@@ -0,0 +1,10 @@
+import turtle
+
+joe = turtle.Turtle() # Define a turtel 
+joe.shape("turtle")
+joe.color("red")
+joe.forward(100)
+
+
+screen = turtle.Screen() # define the screen object
+screen.exitonclick()
