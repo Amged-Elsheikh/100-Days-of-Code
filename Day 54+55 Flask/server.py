4a6cadfd2fd02344620f772718a55477cb9229a6
Amged Elsheikh <62318395+Amged-Elsheikh@users.noreply.github.com>
2022-07-09 00:42:12 +0900

    Add day 54+55

---
+++ Day 54+55 Flask/server.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from random import randint
+
+N = randint(0,9)
+app = Flask(__name__)
+
+@app.route('/')
+def main_page():
+    return """<h1>Guess a number between 0 and 9</h1>
+        <img src="https://media.giphy.com/media/abHQ4aDqRqlyBTdAP0/giphy.gif">"""
+    
+    
+@app.route('/<int:number>')
+def guessed_number(number):
+    if number > N:
+        return """<h1>Too high, try again!</h1>
+    <img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif">"""
+    elif number < N:
+        return """<h1>Too slow, try again!</h1>
+    <img src="https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif">"""
+    else:
+        return """<h1>Correct Answer!</h1>
+    <img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'>"""        
+    
+            
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
