35d0400e81a66e81cdec293eca7cbb47023b6edf
Amged-Elsheikh <amgedelshiekh@gmail.com>
2022-11-12 19:26:13 +0900

    Day 59

---
+++ Day 59 advanced blog/blog.py
@@ -0,0 +1,7 @@
+class Blog:
+    def __init__(self, data):
+        self.id = data['id']
+        self.title = data['title']
+        self.subtitle = data['subtitle']
+        self.body = data['body']
+        
